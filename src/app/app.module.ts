2c5aed092175a41d3dd0c32e5b326d1a6d49f10a
Mohammad <adnan@tuple.tech>
2017-03-13 19:18:24 +0530

    My Website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BlogComponent } from './blog/blog.component';
+
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'blog',
+        component: BlogComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,10 @@
+import { Component, Directive } from '@angular/core';
+
+@Component({
+    selector: 'blog',
+    templateUrl: './blog.component.html'
+})
+
+export class BlogComponent {
+    title = 'Blog';
+}
